583fac73f3244ebb7df831e2863c6ee268f8ab9e
Mahdi Yazdani <mahdiyazdani@msn.com>
2022-11-06 23:26:26 +0300

    Allow specifying regex values to match

--- jquery.conditional.js
+++ jquery.conditional.js
@@ -2,52 +2,55 @@
  * jQuery.Conditional
  * @version   0.1
  * @author    biohzrdmx <github.com/biohzrdmx>
+ * @author.   Mahdi Yazdani (Forked and modified version to support regex value comparison)
  * @requires  jQuery 1.8+
  * @license   MIT
  * @copyright Copyright © 2015 biohzrdmx. All rights reserved.
  */
-;(function($) {
-	$.fn.conditional = function(options) {
-		if (!this.length) { return this; }
-		var opts = $.extend(true, {}, $.conditional.defaults, options);
-		this.each(function() {
+;(function ($) {
+	$.fn.conditional = function (options) {
+		if (!this.length) {
+			return this
+		}
+		var opts = $.extend(true, {}, $.conditional.defaults, options)
+		this.each(function () {
 			var el = $(this),
-				conditional = el.data('conditional');
-			el.on(opts.eventName, function() {
+				conditional = el.data('conditional')
+			el.on(opts.eventName, function () {
 				var value = el.val(),
-					elements = $('[data-condition="'+ conditional +'"]');
+					elements = $('[data-condition="' + conditional + '"]')
 				// Hide all
-				opts.onDeactivate(elements, opts, function() {
+				opts.onDeactivate(elements, opts, function () {
 					// Show the one(s) that match
-					elements.each(function() {
-						var element = $(this);
-						if ( element.data('match') == value ) {
-							opts.onActivate(element, opts);
+					elements.each(function () {
+						var element = $(this)
+						var regex = new RegExp(element.data('match'), 'gi')
+						if (value.match(regex)) {
+							opts.onActivate(element, opts)
 						}
-					});
-
-				});
-			});
-			el.trigger(opts.eventName);
-		});
-		return this;
-	};
+					})
+				})
+			})
+			el.trigger(opts.eventName)
+		})
+		return this
+	}
 	$.conditional = {
 		defaults: {
 			className: 'hide',
 			eventName: 'change',
-			onActivate: function(element, opts) {
-				element.removeClass(opts.className);
+			onActivate: function (element, opts) {
+				element.removeClass(opts.className)
 			},
-			onDeactivate: function(elements, opts, callback) {
-				elements.addClass(opts.className);
-				callback.call();
+			onDeactivate: function (elements, opts, callback) {
+				elements.addClass(opts.className)
+				callback.call()
 			},
-			autoBind: true
-		}
-	};
-	// Auto-binding via HTML5 datas
-	if ( $.conditional.defaults.autoBind ) {
-		$('[data-conditional]').conditional();
+			autoBind: true,
+		},
 	}
-})(jQuery);
+	// Auto-binding via HTML5 datas
+	if ($.conditional.defaults.autoBind) {
+		$('[data-conditional]').conditional()
+	}
+})(jQuery)
